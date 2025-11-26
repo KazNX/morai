f12330ab571e22b904ece3550c63e4f52c2cb79a
Kazys.Stepanas <k_plus_plus@outlook.com>
2025-11-26 18:53:41 +1000

    Documentation comments

--- morai/FibreQueue.hpp
+++ morai/FibreQueue.hpp
@@ -7,10 +7,25 @@ namespace morai
 {
 /// A single threaded fibre queue with priority insertion.
 ///
-/// This queue is for single threaded
+/// A fibre queue is a primarily a FIFO queue. @c Fibre objects are *moved* into the queue using
+/// @c push() and removed with @c pop(). The queue will grow as required as more fibres are pushed.
+/// The number of items is represented by @c size() and @c empty() tests if the fibre has items or
+/// not.
+///
+/// None of these operations are threadsafe. See @c SharedQueue for a thread safe (fixed size)
+/// queue.
+///
+/// Typical usage is for a scheduler to @c push() new fibres added to that scheduler, first
+/// selecting a queue of the appropriate @c priority(). During a scheduler update, the queue is
+/// continually popped - @c pop() - until it returns a an invalid @c Fibre - see @c Fibre::valid().
+///
+/// Copy operations are disabled because @c Fibre objects are move-only.
 class FibreQueue
 {
 public:
+  /// Create a fibre of the given @p priority and initial @p capacity.
+  ///
+  /// @param priority The queue priority. This priority is not used by the queue itself.
   explicit FibreQueue(int32_t priority, uint32_t capacity = 1024u);
   ~FibreQueue();
 
@@ -20,8 +35,10 @@ public:
   FibreQueue(const FibreQueue &) = delete;
   FibreQueue &operator=(const FibreQueue &) = delete;
 
+  /// Returns the priority of this queue. This is not used by the queue.
   [[nodiscard]] int32_t priority() const noexcept { return _priority; }
 
+  /// Return the number of items in the queue.
   [[nodiscard]] std::size_t size() const
   {
     if (_head >= _tail)
@@ -31,17 +48,28 @@ public:
     return _buffer.size() + _head - _tail;
   }
 
+  /// Returns true if the queue is empty.
   [[nodiscard]] bool empty() const { return _head == _tail; }
 
+  /// Returns true if the queue contains a fibre with the given @p id.
+  /// @param id The @c Id to search for. An invalid @c Id always returns false.
   [[nodiscard]] bool contains(const Id &id) const;
 
+  /// Position to insert a fibre into the queue.
+  /// @param fibre The fibre to move into the queue.
+  /// @param position @c PriorityPosition::Back to insert at the back of the queue (FIFO) or
+  /// @c PriorityPosition::Front to insert at the front of the queue (LIFO).
   void push(Fibre &&fibre, PriorityPosition position = PriorityPosition::Back);
 
   /// Pop the next item off the queue.
   [[nodiscard]] Fibre pop();
 
-  bool cancel(const Id &id);
+  /// Cancel a fibre with the given @p id. The fibre immediately terminates.
+  /// @param id The @c Id of the fibre to cancel.
+  /// @return True if the fibre was cancelled.
+  [[nodiscard]] bool cancel(const Id &id);
 
+  /// Clear all fibres from the queue.
   void clear();
 
 private:
--- morai/Id.hpp
+++ morai/Id.hpp
@@ -8,12 +8,25 @@ namespace morai
 using IdValueType = uint64_t;
 constexpr auto InvalidFibreValue = ~static_cast<IdValueType>(0);
 
+/// Id class for a fibre. Each fibre is uniquely identified by an Id.
+///
+/// The Id itself is a wrapper around a shared value which holds the actual id. Conceptually it can
+/// be considered a shared pointer.
+///
+/// The @c ID does not represent a valid fibre if it's value is @c InvalidFibreValue - see
+/// @c valid(). The @c Id can also be used to check if the fibre is still alive or has been
+/// cleaned up and is no longer running - see @c isRunning().
 class Id
 {
 public:
   static constexpr uint64_t RunningBit = 1u;
 
   Id() = default;
+  /// Construct an @c Id with the given value and set its running state.
+  ///
+  /// @param value The id value. Note that the @c RunningBit must not be set to ensure a valid Id.
+  /// In general only the @c Fibre class should set Id values.
+  /// @param running Mark the fibre @c Id as running?
   explicit Id(IdValueType value, bool running = false)
     : ptr_(std::make_shared<IdValueType>(value))
   {
@@ -26,16 +39,20 @@ public:
   Id &operator=(const Id &other) = default;
   Id &operator=(Id &&other) noexcept = default;
 
+  /// Reports the @c Id value. This never reports the @c RunningBit.
   [[nodiscard]] uint64_t id() const noexcept
   {
     return (ptr_) ? (*ptr_ & ~RunningBit) : InvalidFibreValue;
   }
 
+  /// Returns true if this represents a valid @c Id.
   [[nodiscard]] bool valid() const noexcept { return ptr_ && *ptr_ != InvalidFibreValue; }
 
+  /// Returns true if the fibre associated with this @c Id is marked as running.
   [[nodiscard]] bool isRunning() const noexcept { return valid() && (*ptr_ & RunningBit); }
 
 private:
+  /// Set the running state of this @c Id.
   void setRunning(bool is_running) noexcept
   {
     if (valid())
@@ -56,8 +73,15 @@ private:
   std::shared_ptr<IdValueType> ptr_;
 };
 
+/// @c Id equality operator.
 inline bool operator==(const Id &lhs, const Id &rhs) noexcept
 {
   return lhs.id() == rhs.id();
+}
+
+/// @c Id inequality operator.
+inline bool operator!=(const Id &lhs, const Id &rhs) noexcept
+{
+  return lhs.id() != rhs.id();
 }
 }  // namespace morai
